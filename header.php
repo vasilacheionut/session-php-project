ee23c3e54d621b9c3bd8a2abed4f6f5859b48225
vasilacheionut <vasilacheorionut@gmail.com>
2025-05-16 02:32:57 +0200

    first commit

--- header.php
+++ header.php
@@ -6,31 +6,7 @@ include 'auth.php';
 <head>
     <meta charset="UTF-8">
     <title>Aplicație cu sesiuni</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background: #f5f5f5;
-            margin: 20px;
-        }
-        .menu {
-            background: #fff;
-            padding: 15px;
-            border: 1px solid #ddd;
-            margin-bottom: 20px;
-        }
-        .menu ul {
-            list-style: none;
-            padding: 0;
-        }
-        .menu li {
-            display: inline-block;
-            margin-right: 10px;
-        }
-        .menu a {
-            text-decoration: none;
-            color: #0077cc;
-        }
-    </style>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
