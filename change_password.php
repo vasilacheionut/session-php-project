166401d26f2ba672222afbf2368ae45c4efd2b4d
vasilacheionut <vasilacheorionut@gmail.com>
2025-05-16 02:45:34 +0200

    first commit

--- change_password.php
+++ change_password.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include 'header.php';
 
 if (!isset($_SESSION['auth'])) {
@@ -27,17 +27,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     echo $changed ? "Parolă schimbată cu succes. <a href='dashboard.php'>Înapoi</a>" :
-                    "Parolă curentă greșită. <a href='change_password.php'>Încearcă din nou</a>";
+        "Parolă curentă greșită. <a href='change_password.php'>Încearcă din nou</a>";
     exit;
 }
 ?>
 
-<h2>Schimbă parola</h2>
-<form method="post">
-    <input type="password" name="current" required placeholder="Parola actuală"><br>
-    <input type="password" name="new" required placeholder="Noua parolă"><br>
-    <button type="submit">Salvează</button>
-</form>
+<div class="form-container">
+    <h2>Schimbă parola</h2>
+    <form method="post">
+        <label>Parola actuală:<br>
+            <input type="password" name="current" required placeholder="Parola actuală"><br>
+        </label><br><br>
+
+        <label>Noua parolă:<br>
+            <input type="password" name="new" required placeholder="Noua parolă"><br>
+        </label><br><br>
+
+        <button type="submit">Salvează</button>
+    </form>
+</div>
 
 <?php
 require_once "print_session.php";
