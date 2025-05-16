d050e2827ac4a5d3493405b1547e0be5c6f083cb
vasilacheionut <vasilacheorionut@gmail.com>
2025-05-16 02:37:36 +0200

    first commit

--- public_header.php
+++ public_header.php
@@ -4,46 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Aplicație - Public</title>
+    <link rel="stylesheet" href="style.css">
     <style>
         body {
-            font-family: Arial, sans-serif;
-            background: #f5f5f5;
-            margin: 20px;
-            height: 100vh;
             overflow: hidden;
         }
-
-        .container {
-            height: 100%;
-            width: 100%;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            flex-direction: column;
-        }
-
-        .form-container {
-            position: relative;
-            max-width: 500px;
-            width: 100%;
-            max-height: 350px;
-            height: 100%;
-            margin: 0 auto;
-            background: #fff;
-            padding: 20px;
-            border: 1px solid #ccc;
-        }
-
-        h1 {
-            text-align: center;
-        }
-
-        .account{
-            position: absolute;
-            left: 10px;
-            bottom: 10px;
-        }
-
     </style>
 </head>
 
